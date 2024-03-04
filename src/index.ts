b759d4afca763d0e326c8ffa757c90d406074043
black-techie <willbroadnyirenda@gmail.com>
2024-03-04 06:21:08 +0300

    master

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ const port = 6001;
 
 
 const app = new Elysia()
-            .get("/", () => "Hello World!")
-            .listen(port)
+    .get("*", (body) => console.log(body))
+    .post("*", (body) => console.log(body))
+    .listen(port)
 
 
 console.log("Running on port: ", port)
-            
