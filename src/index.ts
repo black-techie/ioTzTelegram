2164810476115b4d6ec6fce62fa9d15cc829d66b
black-techie <willbroadnyirenda@gmail.com>
2024-03-06 02:44:36 +0300

    hello

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { ElysiaLogging } from "@otherguy/elysia-logging";
 import { Elysia, t } from "elysia";
 const port = 6001;
 
@@ -7,6 +8,9 @@ const app = new Elysia()
     .get("*", ({ body }) => { console.log("INCOMING GET: ", body); return {} })
 
     .post("*", async ({ body }) => {console.log("INCOMING POST: ", body);  return {}})
+    .use(
+        ElysiaLogging()
+      )
 
     .listen(port)
 
