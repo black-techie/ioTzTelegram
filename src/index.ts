5a303be040026b64d3214713879a9d07927891ec
black-techie <willbroadnyirenda@gmail.com>
2024-03-06 02:58:57 +0300

    hello

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,59 @@ import { Elysia, t } from "elysia";
 const port = 6001;
 
 
+interface Message {
+    message_id: number;
+    from: {
+        id: number;
+        is_bot: boolean;
+        first_name: string;
+        language_code: string;
+    };
+    chat: {
+        id: number;
+        first_name: string;
+        type: string;
+    };
+    date: number;
+    text: string;
+}
+
+interface BodyType {
+    message: Message;
+}
+
 const app = new Elysia()
 
     .get("*", ({ body }) => { console.log("INCOMING GET: ", body); return {} })
+    .post("*", async ({ body }: { body: any }) => {
+        if (body?.message) {
+            const res = await fetch(`https://api.telegram.org/bot7152328164:AAGzkucP0M9Mtq3QEJQMqwbZd0xkrhZqWIA/sendMessage`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    method: "POST",
+                    chat_id: body.message.chat.id,
+                    text: `Hello ${body.message.from.first_name} 😍`
+                })
+            });
+            const res_1 = await res.json();
+            console.log(res_1)
+            return {
+                res_1
+            }
 
-    .post("*", async ({ body }) => {console.log("INCOMING POST: ", body);  return {}})
+        }
+    })
+
+
     .use(
         ElysiaLogging()
-      )
+    )
 
     .listen(port)
 
 
-console.log("Running on port: ", port)
+console.log("Running on port: ", app.server?.port)
 
