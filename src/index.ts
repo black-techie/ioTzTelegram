cd6a2985ff317cb72a8e494365bc94b3ba9b9cf9
black-techie <willbroadnyirenda@gmail.com>
2024-03-06 01:53:13 +0300

    master

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,14 @@ const app = new Elysia()
     .get("*", ({ body }) => body)
 
     .post("*", ({ body }) => {
+        console.log("INCOMING: ", body)
         const id = body.message.chat.id;
         const text = body.message.text;
         const user = body.message.chat.first_name;
-        fetch(`https://api.telegram.org/bot7152328164:AAGzkucP0M9Mtq3QEJQMqwbZd0xkrhZqWIA/sendMessage?chat_id=${id}&text=${"Greetings"+user}`)
+        return fetch(`https://api.telegram.org/bot7152328164:AAGzkucP0M9Mtq3QEJQMqwbZd0xkrhZqWIA/sendMessage?chat_id=${id}&text=${"Greetings " + user}`)
             .then(res => res.json())
-            .then(res => console.log(`response from sent data : ${res}`))
-        return {}
+            .then(res => console.log("RESPONSE: ", res))
+            .catch(() => ({}));
     }, {
         body: t.Object({
             message: t.Object({
